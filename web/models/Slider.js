41a8d5e1dcc05730e972b43e2668fac4aa6e6cc1
Abdullah Khan <abdullah5601013@gmail.com>
2025-08-01 12:48:12 +0500

    model updated

--- web/models/Slider.js
+++ web/models/Slider.js
@@ -4,29 +4,30 @@ import { sequelize } from "../config/database.js"
 // Define Slider model with shop field
 const Slider = sequelize.define(
   "Slider",
-   {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      description: {
-        type: DataTypes.TEXT,
-        allowNull: true,
-      },
-      isExpanded: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true,
-      },
-      collectionId: {
-        // New field to store the Shopify collection ID
-        type: DataTypes.STRING,
-        allowNull: true, // Allow null as not all slider types will have a collection
-      },
-      shop: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        index: true,
-      },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    sliderType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "center",
+    },
+    isExpanded: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    shop: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      index: true, // Add index for better query performance
+    },
   },
   {
     tableName: "Sliders",
